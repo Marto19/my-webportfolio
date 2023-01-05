fb0231284f158bebe5a54dd7b09dd875ecccdbe6
Martin Trenkov <bigguissss@gmail.com>
2023-01-05 19:18:55 +0200

    Update jss.js

--- jss.js
+++ jss.js
@@ -1,5 +1,25 @@
+function myFunction(x){
+    if(x.matches){
+        document.body.style.zoom="100%";
+    }else{
+    	document.body.style.zoom="80%";
+    }   
+} 
+if (matchMedia) {
+	    const x = window.matchMedia("(max-width: 1020px)");
+        x.addListener(myFunction);
+        myFunction(x);
+}
+
 TweenMax.to(".loader", 2.2, {
     delay: 5,
     top: "-100%",
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".scrollIndicator", 2, {
+    delay: 10,
+    y: -10,
+    opacity: 0,
     ease: Expo.easeInOut
 });
